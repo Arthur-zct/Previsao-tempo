00fe0c818d9dec1d2e8d48f6d1a9849f9de9bc31
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-04-10 23:02:52 -0300

    Forma basica da previsão pronta

--- src/components/PrevisaoSeguinte.js
+++ src/components/PrevisaoSeguinte.js
@@ -2,22 +2,29 @@ import React from 'react'
 import "./PrevisaoSe.css"
 
 
-const PrevisaoSeguinte = ({Previsao}) => {
-    console.log(Previsao)
-  return (
-    <div className='PrevisaoSeguinteDiv'>
-        <div className='Location'>
-            <h3>{Previsao.main.temp.toFixed(1)}</h3>
-        </div>
-        
-        <div className='image'>
-            <div className='sky_ico'>
-              <img src={`http://openweathermap.org/img/wn/${Previsao.weather[0].icon}.png`} alt='desenho da condição atual do céu' />
+const PrevisaoSeguinte = ({ Previsao }) => {
+    const data = new Date(Previsao.dt_txt);
+    const diasDaSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+    const diaSemana = diasDaSemana[data.getDay()];
+
+    return (
+        <div className='PrevisaoSeguinteDiv'>
+            <div>
+                <h4> {diaSemana} </h4>
             </div>
-            <p>{Previsao.weather[0].description}.</p>
-          </div>
-    </div>
-  )
+            <div className='Location'>
+                <h3 className='titleh3'>{Math.round(Previsao.main.temp)}°</h3>
+            </div>
+
+            <div className='image'>
+                <div className='sky_ico'>
+                    <img src={`http://openweathermap.org/img/wn/${Previsao.weather[0].icon}.png`} alt='desenho da condição atual do céu' />
+                </div>
+                {/*<p>{Previsao.weather[0].description}.</p> */}
+                
+            </div>
+        </div>
+    )
 }
 
 export default PrevisaoSeguinte
